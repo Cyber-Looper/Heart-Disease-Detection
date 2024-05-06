18199d16f3bc55e75f0305dfbc1a088342c6de33
Cyber_Looper <sudharcha14@gmail.com>
2024-05-06 15:58:31 +0530

    Update Heart Disease Prediction.py

--- pages/Heart Disease Prediction.py
+++ pages/Heart Disease Prediction.py
@@ -34,7 +34,7 @@ def add_bg_from_local(image_file):
     """,
     unsafe_allow_html=True
     )
-bg_img = add_bg_from_local('Heart_dis_detect\img\heart.jpeg')
+bg_img = add_bg_from_local('/mount/src/heart-disease-detection/heart.jpeg')
 
 
 head_1, head_2, head_3 = st.columns([20,60, 20])
